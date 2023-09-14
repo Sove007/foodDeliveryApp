572fb1b03c433fb036aeb194af977ae67781b62b
Saurav <109144511+Sove007@users.noreply.github.com>
2023-09-14 12:02:40 +0530

    Update Api for Cors free

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -17,7 +17,7 @@ const Body = ({}) => {
 
   async function getResturants() {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6295061&lng=77.030152&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6295061&lng=77.030152&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
     setAllRestaurant(
